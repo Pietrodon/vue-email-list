08074dc3ffb73d001827e50187ba44f1171e3a1e
Pietrodon <donvitopietro00@gmail.com>
2022-03-09 16:34:38 +0100

    stampa delle mail ricevute

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const app = new Vue({
     el:'#app',
     data: {
-
+        email: [],
     },
     created () {
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then (function(risultato){
-              console.log(risultato)
-          })
+        for( let i=0; i<10; i++){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then ((risultato) => {
+                // console.log(risultato.data.response)
+                this.email.push(risultato.data.response)
+            })
+
+        }
+        
     }
+    
 })
