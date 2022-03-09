08d8e55b0e030d1947b11032783edb00a5f30307
Pietrodon <donvitopietro00@gmail.com>
2022-03-09 16:09:58 +0100

    richiesta e controllo risposta

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('prova js')
+const app = new Vue({
+    el:'#app',
+    data: {
+
+    },
+    created () {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then (function(risultato){
+              console.log(risultato)
+          })
+    }
+})
